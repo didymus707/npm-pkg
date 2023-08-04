9c9c5ce9c875d0931b5f04d9e9796202e6a61832
Adewale Orotayo <didymus7007@gmail.com>
2023-08-04 09:14:39 +0100

    refactoring for making an npm pkg

--- src/component/home.js
+++ src/component/home.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-export const Home = () => {
+export const Home = ({ children = "react router link here with content" }) => {
   return (
     <div className="mx-auto w-8/12 md:w-2/5 bg-[#282B34] mt-28 rounded-lg">
       <nav className="text-white w-full pt-4 px-8">
         <ul className="flex">
-          <li className="hover:text-slate-400">
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li className="ml-auto hover:text-slate-400">
-            <Link to={"/subscribers"}>Subscribers</Link>
-          </li>
-          <li className="mx-4 hover:text-slate-400">
-            <Link to={"/subscribe"}>Subscribe</Link>
-          </li>
-          <li className="hover:text-slate-400">
-            <Link to={"/unsubscribe"}>Unsubscribe</Link>
-          </li>
+          <li className="hover:text-slate-400">{children}</li>
+          <li className="ml-auto hover:text-slate-400">{children}</li>
+          <li className="mx-4 hover:text-slate-400">{children}</li>
+          <li className="hover:text-slate-400">{children}</li>
         </ul>
       </nav>
       <div className="text-white">
@@ -31,15 +22,9 @@ export const Home = () => {
         </div>
         <div className="px-12 pt-8 pb-12">
           <ul>
-            <li className="py-4">
-              <Link to={"/subscribers"}>List all subscribers</Link>
-            </li>
-            <li>
-              <Link to={"/subscribe"}>Subscribe to newsletter</Link>
-            </li>
-            <li className="py-4">
-              <Link to={"/unsubscribe"}>Unsubscribe from newsletter</Link>
-            </li>
+            <li className="py-4">{children}</li>
+            <li>{children}</li>
+            <li className="py-4">{children}</li>
             {/* <li>
               <Link>Distribute Newsletter</Link>
             </li> */}
